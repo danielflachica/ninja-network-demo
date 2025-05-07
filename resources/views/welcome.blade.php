0b8c4604797d87b1e0f581fd8c4f03e99e073e58
Daniel Lachica <daniel.lachica82@gmail.com>
2025-05-07 18:57:16 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ninja Network</title>
+    <h1>Welcome to the Ninja Network</h1>
+    <p>Click the button below to view the list of ninjas.</p>
+
+    <a href="#" class="btn">
+        Find Ninjas!
+    </a>
+</head>
+<body>
+    
+</body>
+</html>

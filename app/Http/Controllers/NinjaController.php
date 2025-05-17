d6ed7fcfa9d514d2773234941c82afc76780f972
Daniel Lachica <daniel.lachica82@gmail.com>
2025-05-17 17:37:42 +0800

    Tutorial #17 - Foreign Keys (part 2)

--- app/Http/Controllers/NinjaController.php
+++ app/Http/Controllers/NinjaController.php
@@ -8,13 +8,14 @@ use Illuminate\Http\Request;
 class NinjaController extends Controller
 {
     public function index() {
-        $ninjas = Ninja::orderBy('created_at', 'desc')->paginate(10);
+        // eager loading: query the related dojo once here instead of multiple times on render in the View (i.e. lazy loading)
+        $ninjas = Ninja::with('dojo')->orderBy('created_at', 'desc')->paginate(10);
 
         return view('ninjas.index', ['ninjas' => $ninjas]);
     }
 
     public function show($id) {
-        $ninja = Ninja::findOrFail($id); // findOrFail serves a 404 Error if it fails to find the record
+        $ninja = Ninja::with('dojo')->findOrFail($id); // findOrFail serves a 404 Error if it fails to find the record
 
         return view('ninjas.show', ['ninja' => $ninja]);
     }

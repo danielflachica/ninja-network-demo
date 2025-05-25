8937974f8e0dc97c82d3b2650b0f39e6cb680109
Daniel Lachica <daniel.lachica82@gmail.com>
2025-05-25 09:36:47 +0800

    Auth Tutorial #3 - Register & Login Forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,24 @@
 <x-layout>
-    <h2>Login</h2>
+    <form action="{{ route('login') }}" method="POST">
+        @csrf
+
+        <h2>Login to Your Account</h2>
+
+        {{-- validation errors --}}
+
+        <label for="email">Email:</label>
+        <input
+            type="email"
+            name="email"
+            value="{{ old('email') }}"
+            required>
+
+        <label for="password">Password:</label>
+        <input
+            type="password"
+            name="password"
+            required>
+
+        <button type="submit" class="btn mt-4">Login</button>
+    </form>
 </x-layout>

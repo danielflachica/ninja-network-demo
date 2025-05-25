43525bcf897245be7a3121e6d729720d559ae4d5
Daniel Lachica <daniel.lachica82@gmail.com>
2025-05-25 09:53:55 +0800

    Auth Tutorial #4 - Registering New Users

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,23 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function showRegister() {
+    public function showRegister()
+    {
         return view('auth.register');
     }
 
-    public function showLogin() {
+    public function showLogin()
+    {
         return view('auth.login');
     }
 
-    public function register() {
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
 
+        $user = User::create($validated);
+
+        Auth::login($user);
+
+        return redirect()->route('ninjas.index');
     }
 
-    public function login() {
+    public function login()
+    {
 
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,14 @@
 
         <h2>Register for an Account</h2>
 
-        {{-- validation errors --}}
+        {{-- Validation errors --}}
+        @if($errors->any())
+            <ul class="px-4 py-2 bg-red-100">
+                @foreach($errors->all() as $error)
+                    <li class="my-2 text-red-500">{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
 
         <label for="name">Name:</label>
         <input
